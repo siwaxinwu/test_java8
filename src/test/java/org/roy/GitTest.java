96faa7735712a96e963286e4ee0f8026d447c2e8
dingyawu <yawu.ding@ximalaya.com>
2022-02-01 11:03:22 +0800

    first

---
+++ src/test/java/org/roy/GitTest.java
@@ -0,0 +1,22 @@
+package org.roy;
+
+import org.junit.Test;
+
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class GitTest {
+  /**
+   * 测试atomicinteger
+   *
+   */
+
+  @Test
+  public void testHex4() throws UnsupportedEncodingException {
+    System.out.println("first");
+  }
+
+
+
+}
